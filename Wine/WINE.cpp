8906a8ff9b0144c9bcfbec33ca8ba640ad40014b
Max Pochepets <agreed_note@hotmail.com>
2020-01-17 21:17:01 +0200

    now its really done

--- Wine/WINE.cpp
+++ Wine/WINE.cpp
@@ -4,6 +4,8 @@ WINE::WINE()
 {
     label = "";
     yrs = 0;
+    pair.first.resize(yrs);
+    pair.second.resize(yrs);
 }
 
 WINE::WINE(const char* l, int y)
@@ -30,10 +32,14 @@ WINE::WINE(const char* l, int y, const int yr[], const int bot[])
 void WINE::GetBottles()
 {
     cout << "Enter " << label << " data for " << yrs << " year(s):\n";
+    if (pair.first.size() == 0 and pair.second.size() == 0) {
+        pair.first.resize(yrs);
+        pair.second.resize(yrs);
+    }
     for (int i = 0; i < yrs; i++)
     {
         cout << "Enter a year: ";
-        cin >> pair.first[i];                   // exception thrown!!!!
+        cin >> pair.first[i];
         cout << "Enter bottles for that year: ";
         cin >> pair.second[i];
     }
